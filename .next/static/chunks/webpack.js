b2f44924d3621cc0d2e61a452aa187e0801444f6
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-08-28 16:28:03 -0300

    fix: issue #2 conflict merge

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8718d4e53595ff19"; }
+/******/ 		__webpack_require__.h = function() { return "5fbe76889ff645f0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
